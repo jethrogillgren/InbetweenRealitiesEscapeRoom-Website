f13f3dd0fc5457145ab0ea4fdb558a8a2d37c16d
jethrogillgren <jethrogillgren@gmail.com>
2017-12-13 13:39:16 +0000

    Removed the old overwrite parents file files

--- suevafree-child/overwriteFilesInParent.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-/bin/cp -rf core/templates/header/header-layout-1.php ../../suevafree/core/templates/header/header-layout-1.php
-/bin/cp -rf core/templates/header/logo-layout.php ../../suevafree/core/templates/header/logo-layout.php
-/bin/cp -rf core/main.php ../../suevafree/core/main.php
-
-chown -R nginx:nginx /usr/share/nginx/html
