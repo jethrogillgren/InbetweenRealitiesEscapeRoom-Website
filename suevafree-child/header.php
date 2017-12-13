c4755e96fe2d01bcb587c5003f00f8c41b8166d2
jethrogillgren <jethrogillgren@gmail.com>
2017-12-13 14:03:42 +0000

    Changes for CSS

--- suevafree-child/header.php
+++ suevafree-child/header.php
@@ -21,21 +21,21 @@
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-12" >
-								  <nav class="navbar navbar-custom navbar-transparent navbar-fixed-top one-page" role="navigation">
+									
+								  <!-- Manually copy from Titan-Master which is the authorative version -->
+								  <nav class="navbar navbar-custom navbar-fixed-top one-page" role="navigation">
 									<div class="container">
 									  <div class="navbar-header">
-										<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">Inbetween Realities Blog</a>
+										<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="#">Inbetween Realities Blog</a>
 									  </div>
 									  <div class="collapse navbar-collapse" id="custom-collapse">
 										<ul class="nav navbar-nav navbar-right">
-										  <li><a href="#totop">Home</a></li>
-										  <li><a class="section-scroll" href="#pricing">Prices</a></li>
-										  <li><a class="section-scroll" href="#booking">Book</a></li>
-										  <li><a class="section-scroll" href="blog.inbetweenrealitiesescaperoom.com">Blog</a></li>
+										  <li><a href="http://inbetweenrealitiesescaperoom.com/index.html">Main Site</a></li>
 										</ul>
 									  </div>
 									</div>
 								  </nav>
+									
 								</div>
                             </div>
                         </div>
